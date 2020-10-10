d53aaa673c1b5b64edc4a1a0926c2aab0fbfb776
ekaterina-s <catshavrina@gmail.com>
2020-10-10 22:49:00 +0200

    task_modules

---
+++ js/form.js
@@ -0,0 +1,95 @@
+'use strict';
+(function () {
+//  ищем в разметке элементы для активного/неактивного режима
+  const mapPinMain = document.querySelector('.map__pin--main');
+  const imgMapPinMain = mapPinMain.querySelector('img');
+
+  const MAP_PIN_MAIN_FROM_LEFT = 570;
+  const MAP_PIN_MAIN_FROM_TOP = 375;
+
+  const imgMapPinMainWidth = imgMapPinMain.width;
+  const imgMapPinMainHeight = imgMapPinMain.height;
+
+  // координаты гл метки для неактива
+  const mapPinMainXPositionInactive = MAP_PIN_MAIN_FROM_LEFT + imgMapPinMainWidth / 2;
+  const mapPinMainYPositionInactive = MAP_PIN_MAIN_FROM_TOP + imgMapPinMainHeight / 2;
+
+  // координаты гл метки для актива
+  const mapPinMainXPositionActive = MAP_PIN_MAIN_FROM_LEFT + imgMapPinMainWidth / 2;
+  const mapPinMainYPositionActive = MAP_PIN_MAIN_FROM_TOP + imgMapPinMainHeight;
+
+  //  ищем поле адрес
+  const addressField = document.querySelector('.ad-form__element #address');
+  const roomNumber = document.querySelector('#room_number');
+  const capacity = document.querySelector('#capacity');
+  const capacityOptions = document.querySelectorAll('#capacity option');
+
+  const roomsAvailability = {
+    "1": ["1"],
+    "2": ["1", "2"],
+    "3": ["1", "2", "3"],
+    "100": ["0"]
+  };
+
+  const enableOrDisableForm = (element) => {
+    if (element.length > 1) {
+      for (let i = 0; i < element.length; i++) {
+        if (element[i].hasAttribute('disabled')) {
+          element[i].removeAttribute("disabled");
+        } else {
+          element[i].setAttribute("disabled", "true");
+        }
+      }
+    } else {
+      if (element.hasAttribute('disabled')) {
+        element.removeAttribute("disabled");
+      } else {
+        element.setAttribute("disabled", "true");
+      }
+    }
+
+    return (element);
+  };
+
+  //  объявляем фу-ю добавления координат метки в неактивном сост в поле ввода
+  const fillinInputFieldInactive = (input) => {
+    input.value = `${mapPinMainXPositionInactive}px, ${mapPinMainYPositionInactive}px`;
+
+    return input;
+  };
+
+  fillinInputFieldInactive(addressField);
+  //  объявляем фу-ю добавления координат метки в активном сост в поле ввода
+  const fillinInputFieldActive = (input) => {
+    input.value = `${mapPinMainXPositionActive}px, ${mapPinMainYPositionActive}px`;
+
+    return input;
+  };
+
+  const setDefaultChoice = () => {
+    for (let i = 0; i < capacityOptions.length; i++) {
+      capacity[i].setAttribute("disabled", "true");
+    }
+    capacity[2].removeAttribute("disabled", "true");
+  };
+
+  roomNumber.addEventListener('change', () => {
+    for (let i = 0; i < capacityOptions.length; i++) {
+      if (roomsAvailability[roomNumber.value].indexOf(capacityOptions[i].value) > -1) {
+        capacityOptions[i].removeAttribute("disabled");
+      } else {
+        capacityOptions[i].setAttribute("disabled", "true");
+      }
+    }
+  });
+
+  window.form = {
+    enableOrDisableForm: enableOrDisableForm,
+    fillinInputFieldInactive: fillinInputFieldInactive,
+    fillinInputFieldActive: fillinInputFieldActive,
+    addressField: addressField,
+    setDefaultChoice: setDefaultChoice,
+    mapPinMain: mapPinMain
+  };
+
+})();
--- js/main.js
+++ js/main.js
@@ -1,98 +1,39 @@
 'use strict';
+(function () {
+  const GENERATED_OBJECTS_AMOUNT = 8;
+  const formElements = document.querySelectorAll('.map__filters, .ad-form-header, .ad-form__element');
 
-const GENERATED_OBJECTS_AMOUNT = 8;
-const FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-const PHOTOS = [
-  "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-];
-const ACCOMODATION_TYPES = ['palace', 'flat', 'house', 'bungalow'];
-const CHECKS_IN_OUTS = ['12:00', '13:00', '14:00'];
 
-const generatedHotels = [];
-const template = document.querySelector('#pin').content;
-const mapPin = template.querySelector('.map__pin img');
+  window.data.generateArrayOfHotels(GENERATED_OBJECTS_AMOUNT);
 
-const mapPinWidth = mapPin.width;
-const mapPinHeight = mapPin.height;
+  window.form.enableOrDisableForm(formElements);
 
-const MAP_PIN_FROM_LEFT = 200;
-const MAP_PIN_FROM_TOP = 400;
-const mapPinXPosition = MAP_PIN_FROM_LEFT - mapPinWidth / 2;
-const mapPinYPosition = MAP_PIN_FROM_TOP - mapPinHeight;
+  window.form.fillinInputFieldInactive(window.form.addressField);
 
-const mapPinXFirstCoordinate = 0;
-const mapPinYFirstCoordinate = 130;
-const mapPinYSecondCoordinate = 630;
+  const turnOnActiveMode = () => {
+    document.querySelector('.map').classList.remove('map--faded');
+    document.querySelector('.ad-form').classList.remove('ad-form--disabled');
 
-const generateRandomArrayElement = (array) => {
-  return array[Math.floor(Math.random() * array.length)];
-};
+    window.form.enableOrDisableForm(formElements);
 
-const generateRandomArrayLength = (array) => {
-  const arrayResult = [];
-  const newArray = [];
-  newArray.length = Math.floor(Math.random() * array.length);
-
-  for (let i = 0; i < newArray.length; i++) {
-    arrayResult.push(array[Math.floor(Math.random() * array.length)]);
-  }
-  return arrayResult;
-};
-
-const generateObject = (index) => {
-  const hotel = {
-    author: {
-      avatar: `img/avatars/user0${index + 1}.png`,
-    },
-    offer: {
-      title: 'Объявление',
-      address: 'location.x, location.y',
-      price: 100,
-      type: generateRandomArrayElement(ACCOMODATION_TYPES),
-      rooms: 2,
-      guests: 4,
-      checkin: generateRandomArrayElement(CHECKS_IN_OUTS),
-      checkout: generateRandomArrayElement(CHECKS_IN_OUTS),
-      features: generateRandomArrayLength(FEATURES),
-      description: 'описание',
-      photos: generateRandomArrayLength(PHOTOS),
-    },
-    location: {
-      x: Math.floor(Math.random() * mapPinWidth) + mapPinXFirstCoordinate,
-      y: Math.floor(Math.random() * mapPinYSecondCoordinate) + mapPinYFirstCoordinate
-    }
+    //  вызываем функцию добавления отрисованных элементов во фрагмент кода
+    window.pin.addRenderedPins(window.data.generatedHotels);
   };
-  return hotel;
-};
 
-const generateArrayOfHotels = (amount) => {
-  for (let i = 0; i < amount; i++) {
-    generatedHotels.push(generateObject(i));
-  }
-};
+  window.form.mapPinMain.addEventListener('mousedown', (evt) => {
+    if (evt.which === 1) {
+      turnOnActiveMode();
+      //  вызываем фу-ю заполнения адреса в активе
+      window.form.fillinInputFieldActive(window.form.addressField);
+    }
+  });
 
-generateArrayOfHotels(GENERATED_OBJECTS_AMOUNT);
+  window.form.mapPinMain.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Enter') {
+      turnOnActiveMode();
+    }
+  });
 
-document.querySelector('.map').classList.remove('map--faded');
+  window.form.setDefaultChoice();
 
-const renderDomElements = (object) => {
-  const domElement = template.cloneNode(true);
-  domElement.querySelector('.map__pin').style.left = `${object.location.x + mapPinXPosition}px`;
-  domElement.querySelector('.map__pin').style.top = `${object.location.y + mapPinYPosition}px`;
-  domElement.querySelector('.map__pin img').src = object.author.avatar;
-  domElement.querySelector('.map__pin').alt = object.offer.title;
-
-  return domElement;
-};
-
-const addRenderedPins = (array) => {
-  const fragment = document.createDocumentFragment();
-  for (let i = 0; i < array.length; i++) {
-    fragment.appendChild(renderDomElements(array[i]));
-  }
-  return document.querySelector('.map__pins').appendChild(fragment);
-};
-
-addRenderedPins(generatedHotels);
+})();
